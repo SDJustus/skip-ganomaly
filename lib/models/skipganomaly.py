59c0467ac7220b588f21aa2c519e18dbc9230500
Justus Zipfel <justus.zipfel@tu-dresden.de>
2020-10-13 18:04:10 +0200

    made precision recall and f1 score use the correct pos_label

--- lib/models/skipganomaly.py
+++ lib/models/skipganomaly.py
@@ -239,7 +239,7 @@ class Skipganomaly(BaseModel):
             scores["scores"][scores["scores"] < threshold] = 0
             #print(scores)
             precision, recall, f1_score, support = precision_recall_fscore_support(scores["labels"], scores["scores"],
-                                                                                   average="binary")
+                                                                                   average="binary", pos_label=0)
             conf_matrix = confusion_matrix(scores["labels"], scores["scores"])
             performance = OrderedDict([('Avg Run Time (ms/batch)', self.times), ('AUC', auc), ('precision', precision),
                                        ("recall", recall), ("F1_Score", f1_score), ("conf_matrix", conf_matrix),
