b41e5b19eb6cb5f5b581d4384ff6b2abcfc6ff60
Justus Zipfel <justus.zipfel@tu-dresden.de>
2020-11-04 16:58:05 +0100

    using convex hull instead of pyimsearch ordering function

--- eob_view_on_image.py
+++ eob_view_on_image.py
@@ -6,27 +6,22 @@ import numpy as np
 import cv2
 import os
 import json
+from scipy.spatial.qhull import ConvexHull
+
 
 #https://www.pyimagesearch.com/2014/08/25/4-point-opencv-getperspective-transform-example/
 
 def order_points(pts):
-	# initialzie a list of coordinates that will be ordered
-	# such that the first entry in the list is the top-left,
-	# the second entry is the top-right, the third is the
-	# bottom-right, and the fourth is the bottom-left
-	rect = np.zeros((4, 2), dtype = "float32")
-	# the top-left point will have the smallest sum, whereas
-	# the bottom-right point will have the largest sum
-	s = pts.sum(axis = 1)
-	rect[0] = pts[np.argmin(s)]
-	rect[2] = pts[np.argmax(s)]
-	# now, compute the difference between the points, the
-	# top-right point will have the smallest difference,
-	# whereas the bottom-left will have the largest difference
-	diff = np.diff(pts, axis = 1)
-	rect[1] = pts[np.argmin(diff)]
-	rect[3] = pts[np.argmax(diff)]
-	# return the ordered coordinates
+	# convexHull is more consistent in order the point clockwise
+	cv = ConvexHull(pts)
+	rect_r = cv.vertices
+	# Order by top left point
+	rect_r = [pts[i] for i in rect_r]
+	rect_r = np.array(rect_r)
+	top_left_point = np.where(rect_r[:, 0] + rect_r[:, 1] == min(rect_r[:, 0] + rect_r[:, 1]))[0][0]
+
+	rect = np.append(rect_r[top_left_point:], rect_r[:top_left_point], axis=0)
+
 	return rect
 
 def four_point_transform(image, pts):
